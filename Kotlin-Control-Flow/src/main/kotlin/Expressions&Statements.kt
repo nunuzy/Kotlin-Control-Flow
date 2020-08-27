d0956da614dc2e19b0a2354599766dfd42b46acf
nunuzy <fnukhi@gmail.com>
2020-08-27 21:06:10 +0700

    first commit

---
+++ Kotlin-Control-Flow/src/main/kotlin/Expressions&Statements.kt
@@ -0,0 +1,8 @@
+package ExpressionsAndStatements
+fun main() {
+    val hitung = sum(1 , 1 * 4)
+    print(hitung)
+}
+fun sum(value1: Int, value2: Int) = value1 + value2
+
+
---
+++ Kotlin-Control-Flow/src/main/kotlin/WhileDoWhile.kt
@@ -0,0 +1,7 @@
+fun main(){
+    var counter = 1
+    do{
+        println("Hallo Mas, I Love You 3000")
+        counter ++
+    }while (counter <= 5 )
+}
